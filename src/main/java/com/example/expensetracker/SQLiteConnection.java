d99908a6221f4b4f41477d09a79eb1adaa23a270
Mohana Misra <iammohanamisra@gmail.com>
2023-11-05 16:08:13 +0530

    Modified database address

--- src/main/java/com/example/expensetracker/SQLiteConnection.java
+++ src/main/java/com/example/expensetracker/SQLiteConnection.java
@@ -6,7 +6,7 @@ public class SQLiteConnection {
     public static Connection Connector() {
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:/home/mohana/IdeaProjects/LoginApp/mydatabase.sqlite");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/home/mohana/IdeaProjects/ExpenseTracker/mydatabase.sqlite");
             return conn;
         }
         catch(Exception e) {
